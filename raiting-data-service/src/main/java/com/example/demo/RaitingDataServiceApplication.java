03d6b7b4867165993981559325f6d3f31025360c
Daniel Larsen <larslardan@gmail.com>
2022-01-11 20:55:14 +0700

    Create empty movie-info-service, rating-data-service project

---
+++ raiting-data-service/src/main/java/com/example/demo/RaitingDataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaitingDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaitingDataServiceApplication.class, args);
+	}
+
+}
